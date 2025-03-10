2f32a265013734e2cef9dea929123ff3788810d4
Zarog Omer <40351797-jorsxjamy22@users.noreply.replit.com>
2025-03-10 03:48:37 +0000

    chapter 7

---
+++ .ccls-cache/@home@runner@workspace/lab7-2.cpp
@@ -0,0 +1,14 @@
+//Lab7-2.cpp - Displays the number of years required
+//for a company's sales to reach at least $150,000
+//using a 5.5% annual growth rate. Also displays
+//the sales at that time.
+//Created/revised by <your name> on <current date>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    return 0;
+} //end of main function
